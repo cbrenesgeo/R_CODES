cc0c44e09801f1e73812b8ee125690897a9cfeb4
Cristian Brenes <49688931+cbrenesgeo@users.noreply.github.com>
2026-02-12 18:35:17 -0600

    Update image path and improve index calculations
    De manera automatica el script lee los nombres de las bandas y los reescribe como R, G, B

--- indices_rgb_drone.R
+++ indices_rgb_drone.R
@@ -1,58 +1,56 @@
 library(terra)
 library(ggplot2)
-library(viridis)  # Paleta de colores segura para daltónicos
+library(viridis)
 
-# Definir la ruta de la imagen
-ruta_img <- "/home/cbrenes/GIS_RS/ExG/platanar_clip.tif"
-
-# Cargar la imagen como SpatRaster
+ruta_img <- "/home/cbrenes/GIS/florencia/Va-411-11-2-2026-orthophoto.tif"
 img <- rast(ruta_img)
 
-# Verificar las dimensiones y nombres actuales
-print(img)
-plot(img)
+# 1) Asegurar que sean 3 bandas (RGB) aunque venga con banda extra (alpha, mask, etc.)
+nl <- nlyr(img)
+if (nl < 3) stop("El archivo no tiene al menos 3 bandas (RGB). Tiene: ", nl)
 
-# Renombrar las bandas: según tus datos, el orden es Rojo (R), Verde (G), Azul (B)
+# Si tiene más de 3, nos quedamos con las 3 primeras (lo más común en ortofotos)
+if (nl > 3) {
+  message("El raster tiene ", nl, " bandas. Usaré las 3 primeras como RGB.")
+  img <- img[[1:3]]
+}
+
+# 2) Renombrar automáticamente a R,G,B (independiente del nombre original)
 names(img) <- c("R", "G", "B")
 
-# Calcular índices de vegetación basados en RGB
-GRVI <- (img$R - img$G) / (img$R + img$G)
+# ---- Tu flujo sigue igual ----
+GRVI  <- (img$R - img$G) / (img$R + img$G)
 RGBVI <- (img$G - (img$R * img$B)) / ((img$G * img$G) + (img$R + img$B))
-GLI <- ((img$G - img$R) + (img$G - img$B)) / ((2 * img$G) + img$R + img$B)
-VARI <- (img$G - img$R) / (img$G + img$R - img$B)
+GLI   <- ((img$G - img$R) + (img$G - img$B)) / ((2 * img$G) + img$R + img$B)
+VARI  <- (img$G - img$R) / (img$G + img$R - img$B)
 NGRDI <- (img$G - img$R) / (img$G + img$R)
 
-plot(GLI)
-
-# Lista de índices
 indices <- list(GRVI = GRVI, RGBVI = RGBVI, GLI = GLI, VARI = VARI, NGRDI = NGRDI)
 nombres_indices <- names(indices)
 
-# Definir ruta de salida
-ruta_salida <- "/home/cbrenes/GIS_RS/ExG/"
-
-# Guardar los índices como archivos TIFF
+ruta_salida <- "/home/cbrenes/GIS/florencia/"
 for (i in seq_along(indices)) {
-  writeRaster(indices[[i]], filename = paste0(ruta_salida, nombres_indices[i], ".tif"), overwrite = TRUE)
+  writeRaster(indices[[i]], filename = file.path(ruta_salida, paste0(nombres_indices[i], ".tif")),
+              overwrite = TRUE)
 }
 
-# Función para visualizar mapas con ggplot y paleta segura para daltónicos
 plot_raster <- function(r, title) {
   r_df <- as.data.frame(r, xy = TRUE, na.rm = TRUE)
   colnames(r_df) <- c("x", "y", "value")
   
   ggplot(r_df) +
     geom_raster(aes(x = x, y = y, fill = value)) +
-    scale_fill_viridis(option = "plasma") +  # Paleta segura para daltónicos
+    scale_fill_viridis_c(option = "plasma") +  # continua
     coord_equal() +
     labs(title = title, fill = "Valor") +
     theme_minimal()
 }
 
-# Visualizar la imagen original en RGB
+
 plotRGB(img, r = 1, g = 2, b = 3, stretch = "lin", main = "Imagen RGB Original")
 
-# Visualizar los índices
 for (i in seq_along(indices)) {
+  print(plot_raster(indices[[i]], paste("Índice:", nombres_indices[i])))
+}
   print(plot_raster(indices[[i]], paste("Índice:", nombres_indices[i])))
 }
